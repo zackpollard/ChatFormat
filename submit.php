05bf912714499a4dd23d91fa854bbbdd5565937f
Zack Pollard <iam@zackpollard.pro>
2015-11-20 20:53:08 +0000

    Updated submit.php and view.php to use the config file to create the mysql connection

--- submit.php
+++ submit.php
@@ -8,29 +8,34 @@ include("hashids/HashGenerator.php");
 include("hashids/Hashids.php");
 
 if ($_POST['chat'] != null && $_POST['chat'] != "") {
-	$mysqli = new mysqli();
-	if ($mysqli->connect_errno) {
+
+	$config = parse_ini_file('./config/config.ini');
+	$connection = mysqli_connect($config['host'], $config['username'], $config['password'], $config['dbname'], $config['port']);
+
+    if ($connection->connect_errno) {
 		echo "Could not connect to database.";
 	}
-	$stmt = $mysqli->prepare("SELECT COUNT(*) FROM logs");
+
+    $tablename = $config['tablename'];
+
+	$stmt = $connection->prepare("SELECT COUNT(*) FROM $tablename");
 	$stmt->execute();
 	$stmt->store_result();
 	$stmt->bind_result($numid);
 	$stmt->fetch();
 
 	$hashids = new Hashids\Hashids('1e37h4x0r5', 8);
-	$id = $hashids->encode($numid);
+	$strid = $hashids->encode($numid);
 
 	if (isset($_SERVER["REMOTE_ADDR"])) {
 		$ip = $_SERVER["REMOTE_ADDR"];
 	}
 
-	$stmt = $mysqli->prepare("INSERT INTO logs(strid, log, ip) VALUES (?, ?, ?)");
-	$stmt->bind_param("sss", $id, $_POST['chat'], $ip);
+	$stmt = $connection->prepare("INSERT INTO logs (strid, log, ip) VALUES (?, ?, ?)");
+	$stmt->bind_param("sss", $strid, $_POST['chat'], $ip);
 	$stmt->execute();
 
-	header("Location: " . $id ."/");
-}
-else {
+	header("Location: " . $strid ."/");
+} else {
 	echo "Something went wrong :/";
 }
